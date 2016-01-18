2413e574d39450b47c2a0947f353be639f640f2f
William <WilliamLiang918@gmail.com>
2016-01-18 17:49:42 -0500

    Style images.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
   namespace: "api",
-  host: 'http://localhost:3000',
+  host: 'https://awesome-movies-api.herokuapp.com',
   headers: {
     'X-Api-Key': 'BdLLjmwxgUordMICMHoUcwtt',
   }
